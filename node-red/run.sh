00080b064f68acd70c445b90e577c9cf2b829c1e
kidhasmoxy <kidhasmoxy@users.noreply.github.com>
2017-09-11 12:42:37 -0400

    Change from using addons folder to using config folder
    HASS.IO doesn't seem to have a great way to store config for addons in an easy to access place (since I don't seem to be able to make the data drive public for all containers.)

--- node-red/run.sh
+++ node-red/run.sh
@@ -5,25 +5,25 @@ SSL=$(jq --raw-output ".ssl" $CONFIG_PATH)
 KEYFILE=$(jq --raw-output ".keyfile" $CONFIG_PATH)
 CERTFILE=$(jq --raw-output ".certfile" $CONFIG_PATH)
 
-if [ ! -d /addons/node-red]; then
-	mkdir /addons/node-red
+if [ ! -d /config/node-red]; then
+	mkdir /config/node-red
 fi
-if [ ! -e /addons/node-red/settings.js ]; then
-    cp /settings.js /addons/node-red/settings.js
+if [ ! -e /config/node-red/settings.js ]; then
+    cp /settings.js /config/node-red/settings.js
 fi
 
 # Add ssl configs
 if [ "$SSL" == "true" ]; then
-    sed -i 's/.*var fs = require("fs")/var fs = require("fs")/g' /addons/node-red/settings.js
-    sed -i '/https: {/,/}/ s/\/\///' /addons/node-red/settings.js
-    sed -i "s/.*key: fs.readFileSync('.*'),/        key: fs.readFileSync(\'\/ssl\/$KEYFILE\'),/g" /addons/node-red/settings.js
-    sed -i "s/.*cert: fs.readFileSync('.*')/        cert: fs.readFileSync(\'\/ssl\/$CERTFILE\')/g" /addons/node-red/settings.js
+    sed -i 's/.*var fs = require("fs")/var fs = require("fs")/g' /config/node-red/settings.js
+    sed -i '/https: {/,/}/ s/\/\///' /config/node-red/settings.js
+    sed -i "s/.*key: fs.readFileSync('.*'),/        key: fs.readFileSync(\'\/ssl\/$KEYFILE\'),/g" /config/node-red/settings.js
+    sed -i "s/.*cert: fs.readFileSync('.*')/        cert: fs.readFileSync(\'\/ssl\/$CERTFILE\')/g" /config/node-red/settings.js
 else
-    sed -i 's/.*var fs = require("fs")/\/\/var fs = require("fs")/g' /addons/node-red/settings.js
-    sed -i '/^[ ]*https: {/,/}/ s/^[ ]*/    \/\//' /addons/node-red/settings.js
-    sed -i "s/.*key: fs.readFileSync/    \/\/    key: fs.readFileSync/g" /addons/node-red/settings.js
-    sed -i "s/.*cert: fs.readFileSync/    \/\/    cert: fs.readFileSync/g" /addons/node-red/settings.js
+    sed -i 's/.*var fs = require("fs")/\/\/var fs = require("fs")/g' /config/node-red/settings.js
+    sed -i '/^[ ]*https: {/,/}/ s/^[ ]*/    \/\//' /config/node-red/settings.js
+    sed -i "s/.*key: fs.readFileSync/    \/\/    key: fs.readFileSync/g" /config/node-red/settings.js
+    sed -i "s/.*cert: fs.readFileSync/    \/\/    cert: fs.readFileSync/g" /config/node-red/settings.js
 fi
 
 cd /usr/src/node-red
-npm start -- --userDir /addons/node-red/
+npm start -- --userDir /config/node-red/
